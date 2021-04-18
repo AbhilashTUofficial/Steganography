7acc3c4548331d246606898672b25f6a5ae908ae
AbhilashTUofficial <abhilashstorm@gmail.com>
2021-04-18 12:26:03 +0530

    temporarily postponed

--- src/com/steganography/Main.java
+++ src/com/steganography/Main.java
@@ -1,249 +1,240 @@
 package com.steganography;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Scanner;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class Main extends JFrame implements ActionListener {
-
-    JTabbedPane tabs = new JTabbedPane();
-    JPanel intro = new JPanel();
-    JPanel encode = new JPanel();
-    JPanel decode = new JPanel();
-
-    int winWidth = 600;
-    int winHeight = 600;
-
-    JTextField heading = new JTextField();
-    JTextField encodeHeading = new JTextField();
-    JTextArea encodeInfoHeading = new JTextArea();
-    JTextArea encodeInfoParagraph = new JTextArea();
-    JTextField decodeHeading = new JTextField();
-    JTextArea decodeInfoHeading = new JTextArea();
-    JTextArea decodeInfoParagraph = new JTextArea();
-
-    JButton encodeUploadImg = new JButton();
-    JButton encodeInsertTxt = new JButton();
+    JPanel window = new JPanel();
+    JPanel btnPanel = new JPanel();
+    JTextArea description = new JTextArea();
+    JButton uploadImgBtn = new JButton();
+    JButton uploadTextBtn = new JButton();
     JButton encodeBtn = new JButton();
-
-    JButton decodeUploadImg = new JButton();
-    JButton decodeSaveTxt = new JButton();
     JButton decodeBtn = new JButton();
 
-    ImageIcon defaultImage=new ImageIcon(getClass().getResource("img.jpg"));
-    JLabel defaultImg;
+    File imagePath = null;
+    String textPath = "";
+    String mess="";
 
+    JTextField heading = new JTextField();
 
-    // Fonts
-    Font fontH = new Font("Arial", Font.BOLD, 32);
-    Font fontH2 = new Font("Arial", Font.PLAIN, 24);
-    Font fontP = new Font("Arial", Font.PLAIN, 18);
-
+    BufferedImage sourceImage = null;
+    BufferedImage embeddedImage = null;
 
     Main() {
-        // Window setups
-        setBounds(200, 100, winWidth, winHeight);
+        setBounds(100, 200, 500, 260);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
         setResizable(false);
-        setBackground(Color.DARK_GRAY);
-        add(BorderLayout.CENTER, tabs);
 
-        tabs.addTab("  Introduction  ", intro());
-        tabs.addTab("  Encode  ", encode());
-        tabs.addTab("  Decode  ", decode());
+        window.setBackground(Color.darkGray);
+        window.setForeground(Color.white);
+        window.setLayout(new GridLayout(3, 1));
+        window.setFocusable(false);
+
+        window.add(setHeading());
+        window.add(setButtonPanel());
+        window.add(setDescription());
+        window.setVisible(true);
+        add(window);
         setVisible(true);
     }
 
-    public JPanel intro() {
-        intro.setBackground(Color.darkGray);
-        intro.setFocusable(false);
-        intro.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.darkGray));
-        intro.setLayout(null);
-
-        heading.setForeground(Color.white);
-        heading.setBackground(Color.darkGray);
-        heading.setBounds(0, 0, winWidth, 100);
+    public JTextField setHeading() {
         heading.setHorizontalAlignment(JTextField.CENTER);
-        heading.setFont(fontH);
-        heading.setFocusable(false);
-        heading.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.darkGray));
         heading.setText("STEGANOGRAPHY");
-        heading.setVisible(true);
-
-        encodeInfoHeading.setForeground(Color.white);
-        encodeInfoHeading.setBackground(Color.darkGray);
-        encodeInfoHeading.setBounds(80, 160, 100, 30);
-        encodeInfoHeading.setFont(fontH2);
-        encodeInfoHeading.setFocusable(false);
-        encodeInfoHeading.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.darkGray));
-        encodeInfoHeading.setText("ENCODE");
-        encodeInfoHeading.setVisible(true);
-
-        encodeInfoParagraph.setForeground(Color.white);
-        encodeInfoParagraph.setBackground(Color.darkGray);
-        encodeInfoParagraph.setBounds(80, 200, 200, 300);
-        encodeInfoParagraph.setFont(fontP);
-        encodeInfoParagraph.setFocusable(false);
-        encodeInfoParagraph.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.darkGray));
-        encodeInfoParagraph.setText("To encode a message \ninto an image, choose\nthe image you want to\nuse, enter your text and\n" +
-                "hit the Encode button.\n");
-        encodeInfoParagraph.setVisible(true);
-
-        decodeInfoHeading.setForeground(Color.white);
-        decodeInfoHeading.setBackground(Color.darkGray);
-        decodeInfoHeading.setBounds(340, 160, 100, 30);
-        decodeInfoHeading.setFont(fontH2);
-        decodeInfoHeading.setFocusable(false);
-        decodeInfoHeading.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.darkGray));
-        decodeInfoHeading.setText("DECODE");
-        decodeInfoHeading.setVisible(true);
-
-        decodeInfoParagraph.setForeground(Color.white);
-        decodeInfoParagraph.setBackground(Color.darkGray);
-        decodeInfoParagraph.setBounds(340, 200, 200, 300);
-        decodeInfoParagraph.setFont(fontP);
-        decodeInfoParagraph.setFocusable(false);
-        decodeInfoParagraph.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.darkGray));
-        decodeInfoParagraph.setText("To decode a hidden \nmessage from an image,\njust choose an image\nand hit the Decode\nbutton.");
-        decodeInfoParagraph.setVisible(true);
-
-        intro.add(heading, BorderLayout.CENTER);
-        intro.add(encodeInfoHeading, BorderLayout.CENTER);
-        intro.add(encodeInfoParagraph, BorderLayout.CENTER);
-        intro.add(decodeInfoHeading, BorderLayout.CENTER);
-        intro.add(decodeInfoParagraph, BorderLayout.CENTER);
-
-        return intro;
+        heading.setFont(new Font("Arial", Font.BOLD, 24));
+        heading.setEditable(false);
+        heading.setFocusable(false);
+        heading.setBackground(Color.darkGray);
+        heading.setForeground(Color.white);
+        heading.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.darkGray));
+        return heading;
     }
 
-    public JPanel encode() {
-        encode.setBackground(Color.darkGray);
-        encode.setFocusable(false);
-        encode.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.darkGray));
-        encode.setLayout(null);
-        encode.setVisible(true);
+    public JPanel setButtonPanel() {
 
-        encodeHeading.setForeground(Color.white);
-        encodeHeading.setBackground(Color.darkGray);
-        encodeHeading.setBounds(0, 0, winWidth, 100);
-        encodeHeading.setHorizontalAlignment(JTextField.CENTER);
-        encodeHeading.setFont(fontH);
-        encodeHeading.setFocusable(false);
-        encodeHeading.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.darkGray));
-        encodeHeading.setText("ENCODE");
-//
-//        defaultImg = new JLabel(defaultImage);
-//        defaultImg.setBounds(20, 60, 540, 360);
-//        defaultImg.setVisible(true);
-//        encodeHeading.setVisible(true);
+        btnPanel.setBackground(Color.darkGray);
 
-        encodeUploadImg.setBounds(80, 460, 120, 30);
-        encodeUploadImg.setText("Upload Image");
-        encodeUploadImg.addActionListener(this);
-        encodeUploadImg.setFocusable(false);
-        encodeUploadImg.setVisible(true);
+        uploadImgBtn.setText("Upload Image");
+        uploadImgBtn.setBounds(new Rectangle(100, 40));
+        uploadImgBtn.setFocusable(false);
+        uploadImgBtn.addActionListener(this);
+        uploadImgBtn.setVisible(true);
 
-        encodeInsertTxt.setBounds(220, 460, 120, 30);
-        encodeInsertTxt.setText("Insert Text");
-        encodeInsertTxt.addActionListener(this);
-        encodeInsertTxt.setFocusable(false);
-        encodeInsertTxt.setVisible(true);
+        uploadTextBtn.setText("Upload Text");
+        uploadTextBtn.setBounds(new Rectangle(100, 40));
+        uploadTextBtn.setFocusable(false);
+        uploadTextBtn.addActionListener(this);
+        uploadTextBtn.setVisible(true);
 
-        encodeBtn.setBounds(360, 460, 120, 30);
         encodeBtn.setText("Encrypt");
-        encodeBtn.addActionListener(this);
+        encodeBtn.setBounds(new Rectangle(100, 40));
         encodeBtn.setFocusable(false);
+        encodeBtn.addActionListener(this);
         encodeBtn.setVisible(true);
 
-
-
-        encode.add(encodeHeading);
-       // encode.add(defaultImg);
-        encode.add(encodeUploadImg);
-        encode.add(encodeInsertTxt);
-        encode.add(encodeBtn);
-
-        return encode;
-    }
-
-    public JPanel decode() {
-        decode.setBackground(Color.darkGray);
-        decode.setFocusable(false);
-        decode.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.darkGray));
-        decode.setLayout(null);
-        decode.setVisible(true);
-
-        decodeHeading.setForeground(Color.white);
-        decodeHeading.setBackground(Color.darkGray);
-        decodeHeading.setBounds(0, 0, winWidth, 100);
-        decodeHeading.setHorizontalAlignment(JTextField.CENTER);
-        decodeHeading.setFont(fontH);
-        decodeHeading.setFocusable(false);
-        decodeHeading.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.darkGray));
-        decodeHeading.setText("DECODE");
-//
-//        defaultImg = new JLabel(defaultImage);
-//        defaultImg.setBounds(20, 60, 540, 360);
-//        defaultImg.setVisible(true);
-//        decodeHeading.setVisible(true);
-
-        decodeUploadImg.setBounds(80, 460, 120, 30);
-        decodeUploadImg.setText("Upload Image");
-        decodeUploadImg.addActionListener(this);
-        decodeUploadImg.setFocusable(false);
-        decodeUploadImg.setVisible(true);
-
-        decodeSaveTxt.setBounds(220, 460, 120, 30);
-        decodeSaveTxt.setText("Save Text");
-        decodeSaveTxt.addActionListener(this);
-        decodeSaveTxt.setFocusable(false);
-        decodeSaveTxt.setVisible(true);
-
-        decodeBtn.setBounds(360, 460, 120, 30);
         decodeBtn.setText("Decrypt");
-        decodeBtn.addActionListener(this);
+        decodeBtn.setBounds(new Rectangle(100, 40));
         decodeBtn.setFocusable(false);
+        decodeBtn.addActionListener(this);
         decodeBtn.setVisible(true);
 
 
+        btnPanel.add(uploadImgBtn);
+        btnPanel.add(uploadTextBtn);
+        btnPanel.add(encodeBtn);
+        btnPanel.add(decodeBtn);
+        return btnPanel;
+    }
 
-        decode.add(decodeHeading);
-        //decode.add(defaultImg);
-        decode.add(decodeUploadImg);
-        decode.add(decodeSaveTxt);
-        decode.add(decodeBtn);
-
-        return decode;
+    public JTextArea setDescription() {
+        description.setBackground(Color.darkGray);
+        description.setForeground(Color.white);
+        description.setFont(new Font("Arial", Font.PLAIN, 18));
+        description.setText("Encrypt your test to a image by clicking the Encrypt \nbutton," +
+                "Decode the encrypted message\nfrom image" +
+                "by clicking the Decrypt button");
+        description.setFocusable(false);
+        description.setEditable(false);
+        description.setBorder(BorderFactory.createEmptyBorder(3, 56, 10, 3));
+        description.setVisible(true);
+        return description;
     }
 
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==decodeUploadImg || e.getSource()==encodeUploadImg){
-            System.out.println("upload");
-            JFileChooser fileChooser = new JFileChooser();
-            fileChooser.setCurrentDirectory(new File("."));
-            FileNameExtensionFilter filter = new FileNameExtensionFilter("Image file", "jpg");
-            fileChooser.setFileFilter(filter);
+    private void openImage() {
+        try {
+            sourceImage = ImageIO.read(imagePath);
+            JLabel l = new JLabel(new ImageIcon(sourceImage));
+            System.out.println(sourceImage);
+            this.validate();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-            int response = fileChooser.showOpenDialog(null);
+    private void embedMessage() {
 
-            if (response == JFileChooser.APPROVE_OPTION) {
-                File path=fileChooser.getSelectedFile().getAbsoluteFile();
+        embeddedImage = sourceImage.getSubimage(0, 0,
+                sourceImage.getWidth(), sourceImage.getHeight());
+        embedMessage(embeddedImage, mess);
+       // JLabel l = new JLabel(new ImageIcon(embeddedImage));
+        //embeddedPane.getViewport().add(l);
+        this.validate();
+    }
 
+    private void embedMessage(BufferedImage img, String mess) {
+        int messageLength = mess.length();
 
+        int imageWidth = img.getWidth(), imageHeight = img.getHeight(),
+                imageSize = imageWidth * imageHeight;
+        if (messageLength * 8 + 32 > imageSize) {
+            JOptionPane.showMessageDialog(this, "Message is too long for the chosen image",
+                    "Message too long!", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        embedInteger(img, messageLength, 0, 0);
+
+        byte b[] = mess.getBytes();
+        for (int i = 0; i < b.length; i++)
+            embedByte(img, b[i], i * 8 + 32, 0);
+    }
+
+    private void embedInteger(BufferedImage img, int n, int start, int storageBit) {
+        int maxX = img.getWidth(), maxY = img.getHeight(),
+                startX = start / maxY, startY = start - startX * maxY, count = 0;
+        for (int i = startX; i < maxX && count < 32; i++) {
+            for (int j = startY; j < maxY && count < 32; j++) {
+                int rgb = img.getRGB(i, j), bit = getBitValue(n, count);
+                rgb = setBitValue(rgb, storageBit, bit);
+                img.setRGB(i, j, rgb);
+                count++;
             }
         }
+    }
+
+    private void embedByte(BufferedImage img, byte b, int start, int storageBit) {
+        int maxX = img.getWidth(), maxY = img.getHeight(),
+                startX = start / maxY, startY = start - startX * maxY, count = 0;
+        for (int i = startX; i < maxX && count < 8; i++) {
+            for (int j = startY; j < maxY && count < 8; j++) {
+                int rgb = img.getRGB(i, j), bit = getBitValue(b, count);
+                rgb = setBitValue(rgb, storageBit, bit);
+                img.setRGB(i, j, rgb);
+                count++;
+            }
+        }
+    }
+    private int getBitValue(int n, int location) {
+        int v = n & (int) Math.round(Math.pow(2, location));
+        return v == 0 ? 0 : 1;
+    }
+
+    private int setBitValue(int n, int location, int bit) {
+        int toggle = (int) Math.pow(2, location), bv = getBitValue(n, location);
+        if (bv == bit)
+            return n;
+        if (bv == 0 && bit == 1)
+            n |= toggle;
+        else if (bv == 1 && bit == 0)
+            n ^= toggle;
+        return n;
+    }
+    private void saveImage() {
+        if (embeddedImage == null) {
+            JOptionPane.showMessageDialog(this, "No message has been embedded!",
+                    "Nothing to save", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        java.io.File f = showFileDialog(false);
+        String name = f.getName();
+        String ext = name.substring(name.lastIndexOf(".") + 1).toLowerCase();
+        if (!ext.equals("png") && !ext.equals("bmp") && !ext.equals("dib")) {
+            ext = "png";
+            f = new java.io.File(f.getAbsolutePath() + ".png");
+        }
+        try {
+            if (f.exists()) f.delete();
+            ImageIO.write(embeddedImage, ext.toUpperCase(), f);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    private java.io.File showFileDialog(final boolean open) {
+        JFileChooser fc = new JFileChooser("Open an image");
+        javax.swing.filechooser.FileFilter ff = new javax.swing.filechooser.FileFilter() {
+            public boolean accept(java.io.File f) {
+                String name = f.getName().toLowerCase();
+                if (open)
+                    return f.isDirectory() || name.endsWith(".jpg") || name.endsWith(".jpeg") ||
+                            name.endsWith(".png") || name.endsWith(".gif") || name.endsWith(".tiff") ||
+                            name.endsWith(".bmp") || name.endsWith(".dib");
+                return f.isDirectory() || name.endsWith(".png") || name.endsWith(".bmp");
+            }
+
+            public String getDescription() {
+                if (open)
+                    return "Image (*.jpg, *.jpeg, *.png, *.gif, *.tiff, *.bmp, *.dib)";
+                return "Image (*.png, *.bmp)";
+            }
+        };
+        fc.setAcceptAllFileFilterUsed(false);
+        fc.addChoosableFileFilter(ff);
+
+        java.io.File f = null;
+        if (open && fc.showOpenDialog(this) == fc.APPROVE_OPTION)
+            f = fc.getSelectedFile();
+        else if (!open && fc.showSaveDialog(this) == fc.APPROVE_OPTION)
+            f = fc.getSelectedFile();
+        return f;
     }
 
     public static void main(String[] args) {
@@ -251,4 +242,54 @@ public class Main extends JFrame implements ActionListener {
     }
 
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == uploadImgBtn) {
+            FileNameExtensionFilter extensionFilter = new FileNameExtensionFilter("Only Image Files", "JPG", "JPEG", "PNG", "GIF");
+            JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+
+            fileChooser.setAcceptAllFileFilterUsed(false);
+            fileChooser.addChoosableFileFilter(extensionFilter);
+
+            if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+                imagePath = fileChooser.getSelectedFile();
+            }
+            openImage();
+        }
+        if (e.getSource() == uploadTextBtn) {
+            FileNameExtensionFilter extensionFilter = new FileNameExtensionFilter("Text Files", "txt");
+            JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+
+            fileChooser.setAcceptAllFileFilterUsed(false);
+            fileChooser.addChoosableFileFilter(extensionFilter);
+
+            if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+                textPath = String.valueOf(fileChooser.getSelectedFile());
+                BufferedReader in = null;
+                try {
+                    in = new BufferedReader(new FileReader(textPath));
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                }
+                String line = null;
+                while (true) {
+                    try {
+                        if (!((line = in.readLine()) != null)) break;
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                    mess.concat(line);
+                }
+                try {
+                    in.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+        }
+        if (e.getSource() == encodeBtn) {
+            embedMessage();
+            saveImage();
+        }
+    }
 }
